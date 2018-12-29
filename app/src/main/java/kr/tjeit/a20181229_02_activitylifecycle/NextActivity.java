bc959b063e158855558affe3cfb46c1245f0f06f
yorudaydream <yorudaydream@gmail.com>
2018-12-29 12:15:08 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjeit/a20181229_02_activitylifecycle/NextActivity.java
@@ -0,0 +1,50 @@
+package kr.tjeit.a20181229_02_activitylifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class NextActivity extends BaseActivity {
+    private android.widget.Button closeBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        bindViews();
+        setupEvents();
+        setValues();
+
+        Toast.makeText(mContext, "Next - onCreate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Toast.makeText(mContext, "Next - onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void setupEvents() {
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        this.closeBtn = (Button) findViewById(R.id.closeBtn);
+    }
+}
